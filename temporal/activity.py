8b1bd1c3ac80235efffb2a1e7c7c462b5838cb4e
deight93 <deight8311@gmail.com>
2024-05-07 18:50:50 +0900

    feat: 시나리오에 맞게 temporal 코드 수정

--- temporal/activity.py
+++ temporal/activity.py
@@ -3,7 +3,19 @@ from temporalio import activity
 
 
 @activity.defn
-async def send_request(url: str) -> str:
+async def send_request(url: str, number: int | None = None) -> dict:
+    params = {}
+    if number is not None:
+        params["number"] = number
+
     async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
-            return await response.text()
+        async with session.get(url, params=params) as response:
+            if response.status != 200:
+                raise aiohttp.ClientResponseError(
+                    response.request_info,
+                    response.history,
+                    status=response.status,
+                    message=f"Expected status code 200, but got {response.status}",
+                )
+            data = await response.json()
+            return data
--- temporal/worker.py
+++ temporal/worker.py
@@ -3,15 +3,15 @@ import asyncio
 from activity import send_request
 from temporalio.client import Client
 from temporalio.worker import Worker
-from workflow import RequestWorkflow
+from workflow import AsyncScenario, SyncScenario
 
 
 async def main():
     client = await Client.connect("localhost:7233")
     worker = Worker(
         client,
-        task_queue="request-workflow-task-queue",
-        workflows=[RequestWorkflow],
+        task_queue="request-tasks",
+        workflows=[AsyncScenario, SyncScenario],
         activities=[send_request],
     )
     await worker.run()
